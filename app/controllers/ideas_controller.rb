5d0acb56635380b8ba1d0776907bdba49b6d1f4f
JR Samodal <jrsamodal@hotmail.com>
2020-12-04 18:54:29 -0800

    added a home page and a create page. now adding faker

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,54 @@
+class IdeasController < ApplicationController
+    def index
+        @idea = Idea.all
+    end
+    
+    def new
+        @idea = Idea.new
+        
+    end
+
+    def create
+       
+        @idea = Idea.new params.require(:idea).permit(:title, :body)
+            if @idea.save
+                flash[:notice] = "Blog created successfully"
+                redirect_to idea_path(@idea)
+            else
+                render :new
+            end
+
+               
+    end
+
+    def show
+        @idea = Idea.find params[:id]
+      
+    end
+
+    def edit
+        @idea = Idea.find params[:id]
+       
+    end
+
+    def update
+        @idea = Idea.find params[:id]
+        
+        if @idea.update params.require(:blog).permit(:title, :body)
+            @idea.save
+            redirect_to blog_path(@idea)
+        else
+            render :edit    
+        end
+    
+    end
+
+    def destroy
+        @idea = Idea.find params[:id]
+               @idea.destroy
+               redirect_to root_path
+    end
+
+   
+end   
+
