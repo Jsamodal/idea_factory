8df172678599a66b2f1dc60db9ed794a3f22ad48
JR Samodal <jrsamodal@hotmail.com>
2020-12-04 19:25:42 -0800

    added edit and delete button on show page. now adding a review contoller and db

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -34,9 +34,9 @@ class IdeasController < ApplicationController
     def update
         @idea = Idea.find params[:id]
         
-        if @idea.update params.require(:blog).permit(:title, :body)
+        if @idea.update params.require(:idea).permit(:title, :body)
             @idea.save
-            redirect_to blog_path(@idea)
+            redirect_to idea_path(@idea)
         else
             render :edit    
         end
